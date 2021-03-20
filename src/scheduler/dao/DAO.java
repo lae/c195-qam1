b717cdeeaeb669b1c0cae064a2e42a2ed9f0eef9
Musee Ullah <lae@lae.is>
2021-03-19 22:35:17 -0500

    Add javadoc for scheduler.dao.DAO

--- src/scheduler/dao/DAO.java
+++ src/scheduler/dao/DAO.java
@@ -2,6 +2,11 @@ package scheduler.dao;
 
 import javafx.collections.ObservableList;
 
+/**
+ * A generic interface for our database models.
+ *
+ * @param <T> the application model to create a DAO for.
+ */
 public interface DAO<T> {
     ObservableList<T> listAll();
     T find(T t);
